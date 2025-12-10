616f9dea7856aa0249acc4dd849e3f1fc242b6bb
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2025-12-09 21:59:12 -0300

    feat: adding some photos and our implementation

---
+++ Cartoon-photos-IPI-project/ours_implem.py
@@ -0,0 +1,50 @@
+import skimage
+import skimage.io
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+
+def preproc(contentPath):
+
+    image = skimage.io.imread(contentPath)
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+
+    image = skimage.restoration.denoise_bilateral(image, channel_axis=-1)
+    # aplicando filtro bilateral
+
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+    # agora é necessário aplicar um k-means
+    # o objetivo é reduzir a paleta de cores
+    pixels = image.reshape(-1, 3)
+
+    # O melhor K vai ser o q maximiza o score
+    real_k = 8
+
+    # Aplicar K-means com o melhor k encontrado
+    kmeans = KMeans(n_clusters=real_k, random_state=42, n_init=10)
+    kmeans.fit(pixels)
+
+    # Obter os labels e os centroides dos clusters
+    labels = kmeans.labels_
+    centers = kmeans.cluster_centers_
+
+    # Reconstruir a imagem com a paleta reduzida
+    image_reduced = centers[labels].reshape(image.shape)
+    plt.imshow(image_reduced)
+    plt.axis('off')
+    plt.show()
+
+    edges = skimage.feature.canny(skimage.color.rgb2gray(image), sigma=2)
+    edges = skimage.morphology.dilation(edges)
+    edgeMask = np.invert(edges) # Aplicando o aguçamento na imagem
+    image = image_reduced * edgeMask[:,:,None]
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+    return image_reduced
+
+preproc("patoNaLagoa.jpg")
